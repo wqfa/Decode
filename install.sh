23f01941ee0cb5ddd0deaffad49349268c11b988
XF <118357460+wqfa@users.noreply.github.com>
2023-01-21 23:22:16 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+clear
+pkg install figlet
+clear
+figlet install | lolcat
+sleep 2
+clear
+pkg install nano
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pkg install libcurl
+clear
+gem install lolcat
+clear
+pip2 install uncompyle6
+clear
+pip2 install mechanize
+clear
+pip2 install termocolor
